389cc7edb3842433d1cb278389c2ea52059c7a4b
Thao <t.t.nguyen@live.de>
2022-08-23 13:08:17 +0200

    merge with main

--- src/main/java/com/kbertv/productService/service/CelestialBodyRepository.java
+++ src/main/java/com/kbertv/productService/service/CelestialBodyRepository.java
@@ -7,5 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface CelestialBodyRepository extends MongoRepository<CelestialBody, UUID> {
+public interface
+CelestialBodyRepository extends MongoRepository<CelestialBody, UUID> {
 }
